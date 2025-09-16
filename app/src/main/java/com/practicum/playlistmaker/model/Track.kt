5c5d9419cf13d29cc1f87dc38f607d263e59b1af
denzelandrioki <denzelandrioki@gmail.com>
2025-09-16 15:22:53 +0300

    feat(search): история поиска (SharedPreferences), автопоказ при входе, сохранение темы, правки UI отступов и кнопки

--- app/src/main/java/com/practicum/playlistmaker/model/Track.kt
+++ app/src/main/java/com/practicum/playlistmaker/model/Track.kt
@@ -1,8 +1,9 @@
 package com.practicum.playlistmaker.model
 
 data class Track(
+    val trackId: Long,
     val trackName: String,
     val artistName: String,
-    val trackTime: String,
+    val trackTimeMillis: Long,
     val artworkUrl100: String
 )
